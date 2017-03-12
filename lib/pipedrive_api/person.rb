cad01577bb934dce95f82a2b17714a04bb08c1f0
gareth <hello@garethfuller.me>
2017-03-12 10:06:28 +0000

    Switch to handle res for Person

--- lib/pipedrive_api/person.rb
+++ lib/pipedrive_api/person.rb
@@ -5,12 +5,12 @@ module PipedriveAPI
 
       def find_by_email(email, **params)
         response = get "#{resource_path}/find", query: { term: email, search_by_email: true }.merge(params)
-        response.success? ? response['data'] : bad_response(response, id)
+        handle response
       end
 
       def deals(id, **params)
         response = get "#{resource_path}/#{id}/deals", query: params
-        response.success? ? response['data'] : bad_response(response, id)
+        handle response
       end
 
     end

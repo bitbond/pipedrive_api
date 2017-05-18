797bc03b5affb7c7c377c7c9d6ce10166b731970
Pavel Galeta <superp1987@gmail.com>
2017-05-18 20:12:37 +0300

    duplicate deal

--- lib/pipedrive_api/deal.rb
+++ lib/pipedrive_api/deal.rb
@@ -2,10 +2,10 @@ module PipedriveAPI
   class Deal < Base
 
     class << self
-
-
-
+      def duplicate(id)
+        response = post "#{resource_path}/#{id}/duplicate"
+        handle response
+      end
     end
-
   end
 end
